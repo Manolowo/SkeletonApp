eb6c11b8953ecb9522b4bb02e9165005087a0b84
Manolowo <diego001vm@gmail.com>
2023-08-30 16:40:43 -0400

    SkeletonApp 1.0v

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public alertButtons = ['OK'];
+
+  constructor(private router: Router) { }
+
+  usuario={
+    nombre:"",
+    apellido:""
+  };
+
+  limpiar(){
+    
+  }
 
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  usuario={
+    nombre:"",
+    password:""
+  };
+
+
+  irHome(){
+    return this.router.navigate(['/home']);
+  }
 
   ngOnInit() {
   }
